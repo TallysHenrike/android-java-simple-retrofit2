7dafa4e921d7685af63654f398f0e384f5acbca1
Aluno <aluno@aluno.com>
2019-08-29 21:45:13 -0300

    Consumindo Users

--- app/src/main/java/br/eti/urbano/mobile/exemploretrofit2/resource/PostResource.java
+++ app/src/main/java/br/eti/urbano/mobile/exemploretrofit2/resource/PostResource.java
@@ -2,15 +2,15 @@ package br.eti.urbano.mobile.exemploretrofit2.resource;
 
 import java.util.List;
 
-import br.eti.urbano.mobile.exemploretrofit2.model.Post;
+import br.eti.urbano.mobile.exemploretrofit2.model.Users;
 import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface PostResource {
 
-    @GET("/posts")
-    Call<List<Post>> get();
+    @GET("/users")
+    Call<List<Users>> get();
 
-    @GET("/posts/{id}")
-    Call<List<Post>> get(Integer id);
+    @GET("/users/{id}")
+    Call<Users> get(Integer id);
 }
